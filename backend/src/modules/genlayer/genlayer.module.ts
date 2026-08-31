a28e2d4530a8d50cbf549e137e6e8dc9fdc5c4d5
Aragoorn <alirostami9080@gmail.com>
2026-08-31 20:04:14 +0400

    Delete backend directory

--- backend/src/modules/genlayer/genlayer.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GenlayerService } from './genlayer.service';
-
-@Module({
-  providers: [GenlayerService],
-  exports: [GenlayerService],
-})
-export class GenlayerModule {}
